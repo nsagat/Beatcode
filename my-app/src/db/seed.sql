d94bc0ecb66a1c6079793c64983732953e960709
nsagat <nsagat35@gmail.com>
2025-10-11 13:44:57 -0700

    Database working-schema might need update later

---
+++ my-app/src/db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, email, password_hash, leetcode_id) VALUES
+('64a6bc4f-0e6f-4d11-bcae-087a54fa7f8e', 'NiluferSagat', 'nsagat35@gmail.com', 'abc11111', 'OliveraLotus');
+
--- my-app/src/routes/+page.server.js
+++ my-app/src/routes/+page.server.js
@@ -1,15 +1,23 @@
+/*
 // src/routes/+page.server.js
 import { supabase } from '$lib/supabaseClient';
-
+//import { deleteSessionTokenCookie, invalidateSession } from "$lib/server/session"; // lib/server for google oauth
 export const load = async () => {
   const { data, error } = await supabase
-    .from('instruments')
+    .from('users')
     .select('*'); // or pick columns explicitly
 
   if (error) {
     console.error('Supabase error:', error); // shows exact reason in terminal
-    return { instruments: [], error: error.message };
+    return { users: [], error: error.message };
   }
 
-  return { instruments: data ?? [] };
+  return { users: data ?? [] };
 };
+
+*/
+import { supabase } from '$db/connection';
+export async function load() {
+  const { data, error } = await supabase.from('users').select('*').limit(1);
+  console.log('data', data, 'error', error);
+}
